455bec859c02de2d779c8607e522e59c1eaca532
jsildatk <jakub.sildatk@gmail.com>
2020-10-24 12:13:48 +0200

    Check if password reset token exists for user

--- restauranty-repository/src/main/java/pl/taskyers/restauranty/repository/tokens/PasswordRecoveryTokenRepository.java
+++ restauranty-repository/src/main/java/pl/taskyers/restauranty/repository/tokens/PasswordRecoveryTokenRepository.java
@@ -3,6 +3,7 @@ package pl.taskyers.restauranty.repository.tokens;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.taskyers.restauranty.core.data.tokens.entity.PasswordRecoveryToken;
+import pl.taskyers.restauranty.core.data.users.entity.UserBase;
 
 import java.util.Optional;
 
@@ -10,5 +11,7 @@ import java.util.Optional;
 public interface PasswordRecoveryTokenRepository extends JpaRepository<PasswordRecoveryToken, Long> {
     
     Optional<PasswordRecoveryToken> findByToken(String token);
+    
+    PasswordRecoveryToken findByUser(UserBase user);
     
 }
